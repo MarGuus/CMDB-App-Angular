0a283978660839cf7332c3ba3b2b51e796f68952
MarGuus <markus.a.virtanen@gmail.com>
2022-04-13 19:55:55 +0300

    Added a ci class made ci-list loop through CI list

--- src/app/ci-main/ci-list/ci-list.component.ts
+++ src/app/ci-main/ci-list/ci-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ci } from '../ci.model';
 
 @Component({
   selector: 'app-ci-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ci-list.component.css']
 })
 export class CiListComponent implements OnInit {
+  CIs:Ci[] = [
+    new Ci('A test computer','this is a test', 'https://freesvg.org/img/1545450625.png'),
+    new Ci('Another test computer','this is a test', 'https://freesvg.org/img/1545450625.png')
+  ];
 
   constructor() { }
 
