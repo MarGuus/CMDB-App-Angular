3bc1c145e23a847009c030174c965b38359f5a93
MarGuus <markus.a.virtanen@gmail.com>
2022-04-15 14:53:38 +0300

    added list to order component added placeholder buttons to manage orders list

--- src/app/ci-order/ci-order.component.ts
+++ src/app/ci-order/ci-order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Part } from '../shared/component.model';
 
 @Component({
   selector: 'app-ci-order',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ci-order.component.css']
 })
 export class CiOrderComponent implements OnInit {
-
+  parts:Part[] = [
+    new Part('Kingston Ram 8GB',2),
+    new Part('Asus motherboard',1)
+  ]; 
   constructor() { }
 
   ngOnInit(): void {
